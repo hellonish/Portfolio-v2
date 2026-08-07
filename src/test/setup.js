24cffd9c92fe8fb71e80d61b4d730a5d32c8a538
Nishant Sharma <hellonishantsh@gmail.com>
2026-08-07 18:47:40 -0400

    chore: remove retired portfolio assets

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,5 +1,20 @@
 import { vi } from 'vitest'
 
+const storage = new Map()
+const localStorageMock = {
+  get length() {
+    return storage.size
+  },
+  clear: () => storage.clear(),
+  getItem: (key) => storage.get(String(key)) ?? null,
+  key: (index) => [...storage.keys()][index] ?? null,
+  removeItem: (key) => storage.delete(String(key)),
+  setItem: (key, value) => storage.set(String(key), String(value)),
+}
+
+Object.defineProperty(window, 'localStorage', { configurable: true, value: localStorageMock })
+Object.defineProperty(globalThis, 'localStorage', { configurable: true, value: localStorageMock })
+
 global.ResizeObserver = class {
   observe() {}
   unobserve() {}
